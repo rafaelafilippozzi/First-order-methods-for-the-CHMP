b51da059823719171940af9ae403a22fd3ec4178
rafaelafilippozzi <93533657+rafaelafilippozzi@users.noreply.github.com>
2021-11-01 13:56:36 -0300

    Add files via upload

---
+++ simplex_proj.m
@@ -0,0 +1,28 @@
+function [x] = simplex_proj(a)
+%% Projection a vector 'a' onto the unit simplex
+%
+% Syntax: 
+%        [x] = simplex_proj(a)
+%
+% Reference: C. Michelot. JOTA, 1986 
+
+% size of a
+n=length(a);
+x=a;
+I = 1:n;
+
+for t=1:n
+    d = length(I);    
+    % projection on V_I
+    x(I) = x(I) + (1/d)*(1 - sum(x(I)));    
+    N = find(x<0);    
+    % stopping criterion
+    if isempty(N)
+        break;
+    end
+    % projection on X_I
+    x(N) = 0;   
+    % update I
+    I=find(x>0);    
+end
+
