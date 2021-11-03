16ba4a22974a9a4816b821ac5b4eed4858a043f9
rafaelafilippozzi <rafaela.filippozzi@gmail.com>
2021-11-03 15:50:51 -0300

    experimentsnews

--- ExperimentsSection6_1.m
+++ ExperimentsSection6_1.m
@@ -1,13 +1,17 @@
 %Experiments_section_6_1
+addpath ('Functions')
+addpath ('Algorithms')
 Titles = ['Case a'; 'Case b'; 'Case c'; 'Case d'];
 Size = ['pequeno'; 'grande '];
+mkdir visitordata6-1;
 for pvar = 1:4
-    for nvar = 1:2 
+    for nvar = 1:2                
         [averagetime, averageiteration, parameters, countfail] = artificialchmp(nvar,pvar);
         [n] = generatesgraphics(pvar,nvar,averagetime,countfail);
         title(Titles(pvar,:));
         hold off
-        save ([Titles(pvar,:),Size(nvar,:)])
-        saveas(gcf,[Titles(pvar,:),Size(nvar,:)])    
+        namesave = fullfile('visitordata6-1',[Titles(pvar,:),Size(nvar,:)]);
+        save (namesave);
+        saveas(gcf,namesave)    
     end 
 end
--- ExperimentsSection6_2.m
+++ ExperimentsSection6_2.m
@@ -1,4 +1,6 @@
 %Experiments_section_6_2
+addpath ('Functions')
+addpath ('Algorithms')
 m = [50; 50; 100;200];
 n = [200; 2000; 500 ;2000];
 tolfix = [1e-6; 1e-7];
@@ -7,11 +9,13 @@ Titlesm = ['m_50__'; 'm_50__'; 'm_100_'; 'm_200_'];
 Titlesn = ['n_200__'; 'n_2000_'; 'n_500__'; 'n_2000_'];
 Titlesviability = ['feasible__'; 'infeasible'];
 Titletol = ['_1e-6';'_1e-7'];
+mkdir visitordata6-2;
 for i= 1:4  
     for j = 2 
         for k = 1:2
             [averagetime, averageiteration, parameters, countfail] =  artificialfeasibilityproblem (m(i),n(i),viability(j), tolfix(k));  
-            save ([Titlesm(i,:),Titlesn(i,:),Titlesviability(j,:),Titletol(k,:)])
+            namesave = fullfile('visitordata6-2',[Titlesm(i,:),Titlesn(i,:),Titlesviability(j,:),Titletol(k,:)]);
+            save (namesave);
         end       
     end
 end
